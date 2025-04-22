e36e838becd21d59c98ef79d652c9588ab08c476
Jason Ngo <jason.n.ngo@gmail.com>
2025-04-22 14:03:04 -0400

    Initial Commit

---
+++ JhatBot/JhatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  JhatBotApp.swift
+//  JhatBot
+//
+//  Created by Jason Ngo on 2025-04-22.
+//
+
+import SwiftUI
+
+@main
+struct JhatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JhatBotTests/JhatBotTests.swift
@@ -0,0 +1,17 @@
+//
+//  JhatBotTests.swift
+//  JhatBotTests
+//
+//  Created by Jason Ngo on 2025-04-22.
+//
+
+import Testing
+@testable import JhatBot
+
+struct JhatBotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
