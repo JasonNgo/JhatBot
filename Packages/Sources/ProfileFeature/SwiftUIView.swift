e1fe56299db3e64c852b4b3fbc359860b5f25c98
Jason Ngo <jason.n.ngo@gmail.com>
2025-04-28 20:40:49 -0400

    [feat] Add the user model

---
+++ Packages/Sources/ProfileFeature/SwiftUIView.swift
@@ -0,0 +1,60 @@
+//
+//  UserModel.swift
+//  Main
+//
+//  Created by Jason Ngo on 2025-04-28.
+//
+
+import Foundation
+
+public struct UserModel {
+    public let userId: String
+    public let dateCreated: Date?
+    public let didCompleteOnboarding: Bool?
+    public let profileColorHex: String?
+
+    public init(
+        userId: String,
+        dateCreated: Date?,
+        didCompleteOnboarding: Bool?,
+        profileColorHex: String?
+    ) {
+        self.userId = userId
+        self.dateCreated = dateCreated
+        self.didCompleteOnboarding = didCompleteOnboarding
+        self.profileColorHex = profileColorHex
+    }
+}
+
+// MARK: - Mocks
+
+public extension UserModel {
+    static var mock: Self { mocks[0] }
+
+    static var mocks: [Self] {[
+        UserModel(
+            userId: "user_001",
+            dateCreated: Date(timeIntervalSince1970: 1_687_000_000),
+            didCompleteOnboarding: true,
+            profileColorHex: "#FF5733"
+        ),
+        UserModel(
+            userId: "user_002",
+            dateCreated: Date(timeIntervalSince1970: 1_687_100_000),
+            didCompleteOnboarding: false,
+            profileColorHex: "#33C1FF"
+        ),
+        UserModel(
+            userId: "user_003",
+            dateCreated: nil,
+            didCompleteOnboarding: nil,
+            profileColorHex: "#8E44AD"
+        ),
+        UserModel(
+            userId: "user_004",
+            dateCreated: Date(timeIntervalSince1970: 1_687_200_000),
+            didCompleteOnboarding: true,
+            profileColorHex: nil
+        )
+    ]}
+}
